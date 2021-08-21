9630f04c9c6030a30f3550bd8e5cee63aa784867
Ganesh Madhav Raghupathy <graghupa@usc.edu>
2021-08-21 08:09:19 -0500

    springboot controller api creation

--- src/main/java/com/example/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/example/conferencedemo/ConferenceDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConferenceDemoApplication {
 
 	public static void main(String[] args) {
+		var hello = 2;
 		SpringApplication.run(ConferenceDemoApplication.class, args);
 	}
 
---
+++ src/main/java/com/example/conferencedemo/dao/PersonDao.java
@@ -0,0 +1,25 @@
+package com.example.conferencedemo.dao;
+
+import com.example.conferencedemo.models.Person;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PersonDao {
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+    List<Person> selectAllPeople();
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
+
+    Optional<Person> selectPersonById(UUID id);
+
+}
